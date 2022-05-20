db89e5ac1c9ecc8346d8fdbbf710bfc372c33355
Ido Barel <55856610+idobarel@users.noreply.github.com>
2022-05-20 18:01:42 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 wget https://raw.githubusercontent.com/idobarel/emoji-fatch/main/main.py
 wget https://raw.githubusercontent.com/idobarel/emoji-fatch/main/requirements.txt
+sudo apt-get install dos2unix
 pip3 install -r requirements.txt
 mv main.py ef
+dos2unix ef
 chmod +x ef
 sudo mv ef /usr/local/bin
 clear
