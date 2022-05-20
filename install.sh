b39e356795135057b1917964bf8859f563882112
Ido Barel <55856610+idobarel@users.noreply.github.com>
2022-05-20 17:56:23 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 wget https://raw.githubusercontent.com/idobarel/emoji-fatch/main/main.py
+wget https://raw.githubusercontent.com/idobarel/emoji-fatch/main/requirements.txt
+pip3 install -r requirements.txt
 mv main.py ef
 chmod +x ef
 sudo mv ef /usr/local/bin
